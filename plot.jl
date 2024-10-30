59a5943ab8b35eb25fd0edf9e65e996b976a0ff4
germyhou <147078627+germyhou@users.noreply.github.com>
2024-10-30 00:05:52 -0400

    Add files via upload

--- plot.jl
+++ plot.jl
@@ -3,13 +3,13 @@ using DataFrames
 using Plots
 using DelimitedFiles
 
-# dat = CSV.read("output.csv", DataFrame)
-# p = plot(dat.time, dat.data, seriestype = :line, title = "Corr Function", xlabel = "log(t)", ylabel = "log|C(t)|")
-# savefig(p, "test.png")
+dat = CSV.read("output.csv", DataFrame)
+p = plot(dat.time, dat.data, seriestype = :line, title = "Corr Function", xlabel = "log(t)", ylabel = "log|C(t)|")
+savefig(p, "test.png")
 
-data = readdlm("time_correlation_data.txt", ' ')  # Replace ' ' with '\t' for tab-delimited, or ',' for comma-delimited
-x = data[1:end, 1] 
-y = data[1:end, 2]
-y = abs.(y)
-plot(x, y, seriestype = :scatter, title = "Scatter Plot", xlabel = "X Axis", ylabel = "Y Axis")
-savefig("plot_from_txt.png")
+# data = readdlm("time_correlation_data.txt", ' ')  # Replace ' ' with '\t' for tab-delimited, or ',' for comma-delimited
+# x = data[1:end, 1] 
+# y = data[1:end, 2]
+# y = abs.(y)
+# plot(x, y, seriestype = :scatter, title = "Scatter Plot", xlabel = "X Axis", ylabel = "Y Axis")
+# savefig("plot_from_txt.png")
